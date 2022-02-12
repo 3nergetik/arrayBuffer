0ccede40d96033c0e575594c310dd59e5ce4ca9a
3nergetik <carbo_r86@mail.ru>
2022-02-12 14:43:16 +0500

    2

--- src/arrayBufferConverter.js
+++ src/arrayBufferConverter.js
@@ -1,14 +1,14 @@
 export default class ArrayBufferConverter {
-    load(buffer) {
-      this.buffer = buffer;
-    }
-  
-    toString() {
-      const view = new Uint16Array(this.buffer);
-      let result = '';
-      for (const element of view) {
-        result += String.fromCharCode(element);
-      }
-      return result;
-    }
+  load(buffer) {
+    this.buffer = buffer;
   }
+
+  toString() {
+    const view = new Uint16Array(this.buffer);
+    let result = '';
+    for (const element of view) {
+      result += String.fromCharCode(element);
+    }
+    return result;
+  }
+}
